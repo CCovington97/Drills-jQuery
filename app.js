31f7ebc0f7f69037b1c2fa9deb3932fec901acad
Connor <ccovington439@gmail.com>
2020-01-27 15:35:23 -0600

    steps 5, 6, and 7 completed

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
     let btnSubmit = $('#btnSubmit');
+    let btnText = $('#btnText');
+    let textInput = $('#textInput')
 
     btnSubmit.click(function () {
-        alert('')
-    })
-});
+        alert('The Almighty Button has been clicked')
+    });
+
+    btnText.click(function () {
+        alert(textInput.val());
+        textInput.keyup();
+    });
+
+    textInput.keyup(() => {
+        if (textInput.val() === "") {
+            btnText.attr('disabled', true);
+        } else {
+            btnText.attr('disabled', false);
+        }
+    });
+}
+);
 
